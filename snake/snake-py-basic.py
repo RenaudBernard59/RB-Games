5fbcedcaa78050ffa456404ccbac9599bdd1d8b7
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-17 13:53:49 +0100

    Starting project
    Starting the game project.

--- snake/snake-py-basic.py
+++ snake/snake-py-basic.py
@@ -1,2 +1,32 @@
 #!/usr/bin/enb python
 # coding: utf-8
+###################################
+# Jeu Python
+# Version Py : python3
+# Derrière Mise à jour 17/01/2017
+###################################
+
+__author__ = "Renaud BERNARD"
+__copyright__ = "Copyright 2018"
+__license__ = "Free"
+__version__ = "1.0.1"
+__email__ = "renaud.bernard@p59production.com"
+__status__ = "Production"
+
+######################################
+#Initialise project
+
+
+
+######################################
+#Def Main
+
+def main():
+    """
+    Main components of the program.
+    :return:
+    """
+
+
+
+

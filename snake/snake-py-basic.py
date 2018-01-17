3acd9ea9c13570b5dd702da7c4cb8a4427f0bed3
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-17 12:27:36 +0100

    Debogue
    Deboging code but it will don't run correctly : Raspberry don't appear, snake is redColor, border as mindfuck...
    I will try something different.

--- snake/snake-py-basic.py
+++ snake/snake-py-basic.py
@@ -22,19 +22,19 @@ fpsClock = pygame.time.Clock()
 
 ######################################
 #Graphisme de ouf NVIDIA Geforce 2160Ti
-playSurface = pygame.display.set_mode((800, 600)) #La surface de jeu
+playSurface = pygame.display.set_mode((640, 480)) #La surface de jeu
 pygame.display.set_caption("Python Snake Basic") #Le nom du jeu
 #Définition des couleurs
-redColour = pygame.Color(255, 0, 0)
-blackColour = pygame.Color(0, 0, 0)
-whiteColour = pygame.Color(255, 255, 255)
-greyColour = pygame.Color(150, 150, 150)
+redColour = pygame.Color(255,0,0)
+blackColour = pygame.Color(0,0,0)
+whiteColour = pygame.Color(255,255,255)
+greyColour = pygame.Color(150,150,150)
 
 ######################################
 #Let's moove
-snakePosition = [100, 100]
-snakeSegments[(100, 100), (80, 100), (60, 100)]
-raspeberryPosition = [300, 300] #Pas trop utile
+snakePosition = [100,100]
+snakeSegments = [(100,100),(80,100),(60,100)]
+raspberryPosition = [300,300]
 raspberrySpawned = 1
 direction = "right"
 changeDirection = direction
@@ -43,11 +43,11 @@ changeDirection = direction
 #Functions
 
 def gameOver():
-    gameOverFont = pygame.font.Font('freesansbold.ttf', 100) #Vérifer police
-    gameOverSurf = gameOverFont.render("Game over \n Le serpent s'est mordu la queue", True, greyColor)
+    gameOverFont = pygame.font.Font('freesansbold.ttf', 20) #Vérifer police
+    gameOverSurf = gameOverFont.render("Game over ! Le serpent s'est mordu la queue", True, greyColour)
     gameOverRect = gameOverRect = gameOverSurf.get_rect()
-    gameOverRect.midtop=(400, 30)
-    play.Surface.blit(gameOverSurf, gameOverRect)
+    gameOverRect.midtop=(320,10)
+    playSurface.blit(gameOverSurf, gameOverRect)
     pygame.display.flip()
     time.sleep(5)
     pygame.quit()
@@ -55,11 +55,11 @@ def gameOver():
 #END func gameOver
 
 while True:
-    for even in pygame.event.get():
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type ==KEYDOWN: #Vérifier AZRETY-QUERTY
+        elif event.type == KEYDOWN: #Vérifier AZRETY-QUERTY
             #Direction quand on appuie sur les touches
             if event.key == K_RIGHT or event.key == ord('d'):
                 changeDirection = "right"
@@ -83,11 +83,11 @@ while True:
             #gestion des distances
             if direction == "right":
                 snakePosition[0] +=20
-            if direction == "right":
+            if direction == "K_LEFT":
                 snakePosition[0] -=20
-            if direction == "right":
+            if direction == "up":
                 snakePosition[1] -=20
-            if direction == "right":
+            if direction == "down":
                 snakePosition[1] +=20
             #BodySnake
             snakeSegments.insert(0, list(snakePosition))
@@ -95,26 +95,26 @@ while True:
             if snakePosition[0] == raspberryPosition[0] and snakePosition[1] == raspberryPosition[1]:
                 raspberrySpawned = 0
             else:
-                snakeSegment.pop()
+                snakeSegments.pop()
             #Un nouveau miammiam random
             if raspberrySpawned == 0:
-                x = random.randrange(1,6)
-                y = random.ranrange(1,8)
-                raspberryPosition = [int(x*100), int(y*100)]
+                x = random.randrange(1,32)
+                y = random.ranrange(1,24)
+                raspberryPosition = [int(x*20), int(y*20)]
                 raspberrySpawned = 1
             #Redessiner le bordel
-                playSurface(blackColour)
+                playSurface.fill(blackColour)
             for position in snakeSegments:
                 pygame.draw.rect(playSurface,whiteColour,Rect(position[0],position[1],20,20))
                 pygame.draw.rect(playSurface,redColour,Rect(position[0],position[1],20,20))
                 pygame.display.flip()
             #Kill all snakes
-            if snakePosition[0] > 800 or snakePosition[0] < 0:
+            if snakePosition[0] > 620 or snakePosition[0] < 0:
                 gameOver()
-            if snakePosition[1] > 600 or snakePosition[1] < 0:
+            if snakePosition[1] > 460 or snakePosition[1] < 0:
                 gameOver()
             #Je me suis mordu la queue
-            for snakeBody in snakeSegments[1]:
+            for snakeBody in snakeSegments[1:]:
                 if snakePosition[0] == snakeBody[0] and snakePosition[1] == snakeBody[1]:
                     gameOver()
             #60FPS 4K 3D HDR VR
